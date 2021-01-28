10bbc4ec94d1b49a7e2c4b99e1b9ff3b0c86712d
sanakhanbmj <sana.khan@bmj.com>
2021-01-28 21:20:51 +0530

    changes in page aafter rendering

--- src/templates/page.js
+++ src/templates/page.js
@@ -4,7 +4,7 @@ import {decode} from 'html-entities';
 
 class PageTemplate extends Component {
     render() {
-        const siteMetadata = this.props.data.site.siteMetadata
+       
         const currentPage = this.props.data.wordpressPage
 
         console.log(currentPage)
@@ -13,8 +13,8 @@ class PageTemplate extends Component {
              <Layout>
             <div>
                 <h1 dangerouslySetInnerHTML={{__html: currentPage.title}}/>
-                <div>
-                 {decode(currentPage.content)}
+                <div dangerouslySetInnerHTML={{__html: currentPage.content}}>
+                 
                  </div>
 
                 <p dangerouslySetInnerHTML={{__html: currentPage.date}} />
